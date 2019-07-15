c9673554d1902a20470ffe5c6d313bfc8472ea0f
clstokes <cameron@cameronstokes.com>
2019-07-15 13:06:52 -0700

    update per joe's latest blog

--- README.md
+++ README.md
@@ -1,26 +1,49 @@
+> :information_source: Based on https://www.pulumi.com/blog/unit-testing-infrastructure-in-nodejs-and-mocha/.
+
 # TypeScript Unit Testing with Pulumi
 
 ## To Run Tests
 
 ```
-$ npm run test
+$ pulumi preview
 ```
 
 ### Example Output
 
 ```
-$ npm run test
+$ pulumi preview
+Previewing update (dev):
 
-> aws-ts-ruby-on-rails@0.1.0 test /Users/clstokes/cc/clstokes/pulumi-aws-ts-ruby-on-rails-unit-testing
-> PULUMI_TEST_MODE=true PULUMI_NODEJS_PROJECT=aws-ts-ruby-on-rails PULUMI_NODEJS_STACK=fake-stack PULUMI_CONFIG='{ "aws:region": "us-west-2", "aws-ts-ruby-on-rails:dbUser": "admin2", "aws-ts-ruby-on-rails:dbPassword": "pass2", "aws-ts-ruby-on-rails:dbRootPassword": "pass2" }' mocha -r ts-node/register *.spec.ts
+     Type                      Name                                          Plan       Info
+ +   pulumi:pulumi:Stack       pulumi-aws-ts-ruby-on-rails-unit-testing-dev  create     1 error; 22 messages
+ +   ├─ aws:ec2:SecurityGroup  webServerSecurityGroup                        create
+ +   └─ aws:ec2:Instance       webServer                                     create
 
+Diagnostics:
+  pulumi:pulumi:Stack (pulumi-aws-ts-ruby-on-rails-unit-testing-dev):
+    Running Mocha Tests: /Users/clstokes/cc/clstokes/pulumi-aws-ts-ruby-on-rails-unit-testing/tests/tests.spec.ts
+      Infrastructure
+        #server
+          1) must have a name tag
+        #group
+          2) must not have public internet access
+      0 passing (2s)
+      2 failing
+      1) Infrastructure
+           #server
+             must have a name tag:
+         AssertionError: expected undefined not to be undefined
+          at Object.<anonymous> (tests/tests.spec.ts:20:55)
+          at Generator.next (<anonymous>)
+          at fulfilled (tests/tests.spec.ts:4:58)
+      2) Infrastructure
+           #group
+             must not have public internet access:
+         AssertionError: expected { Object (protocol, fromPort, ...) } to be undefined
+          at Suite.<anonymous> (tests/tests.spec.ts:29:45)
+          at Generator.next (<anonymous>)
+          at fulfilled (tests/tests.spec.ts:4:58)
 
-
-  Infrastructure
-    #server
-      ✓ must have a name tag
-
-
-  1 passing (267ms)
+    error: an unhandled error occurred: Program exited with non-zero exit code: 1
 
 ```
--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-
 import * as config from "./config";
+import { createUserData, renderConfigFile } from "pcloudinit";
+
+import "./tests";
+import { runTests } from "./tests";
 
 export const webSg = new aws.ec2.SecurityGroup("webServerSecurityGroup", {
     description: "Enable HTTP and SSH access",
@@ -9,18 +12,129 @@ export const webSg = new aws.ec2.SecurityGroup("webServerSecurityGroup", {
         { protocol: "-1", fromPort: 0, toPort: 0, cidrBlocks: ["0.0.0.0/0"] },
     ],
     ingress: [
-        { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: [config.sshLocation] },
-        { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
+        // Comment out the line below to to fix test failure.
+        { protocol: "-1", fromPort: 0, toPort: 0, cidrBlocks: ["0.0.0.0/0"] },
     ],
 });
 
+export const amiId = pulumi.output(aws.getAmi({
+    filters: [
+        {
+            name: "name",
+            values: ["amzn-ami-hvm-2018.03*"],
+        },
+        {
+            name: "virtualization-type",
+            values: ["hvm"]
+        }
+    ],
+    mostRecent: true,
+    owners: ["137112412989"],
+}));
+
 export const webServer = new aws.ec2.Instance("webServer", {
-    ami: "ami-0475f60cdd8fd2120",
+    ami: amiId.id,
     instanceType: config.instanceType,
     securityGroups: [webSg.name],
-    keyName: config.keyName,
-    userData: "key1=val1",
+    tags: {
+        // Uncomment the line below to to fix test failure.
+        // Name: "webserver",
+    },
+    userData: createUserData(
+        ["install_ruby_2_3_1", "install_mysql", "configure_mysql", "install_application"],
+        {
+            "install_ruby_2_3_1": {
+                files: {
+                    "/tmp/install_ruby": {
+                        content: renderConfigFile("./files/install_ruby", config),
+                        mode: "000500",
+                        owner: "root",
+                        group: "root",
+                    },
+                },
+                commands: {
+                    "01_install_ruby": {
+                        command: "/tmp/install_ruby > /var/log/install_ruby.log",
+                    },
+                },
+            },
+            "install_mysql": {
+                packages: {
+                    yum: ["mysql", "mysql-server", "mysql-devel", "mysql-libs"],
+                },
+                files: {
+                    "/tmp/setup.mysql": {
+                        content: renderConfigFile("./files/setup.mysql", config),
+                        mode: "000400",
+                        owner: "root",
+                        group: "root",
+                    },
+                },
+                services: {
+                    "sysvinit": {
+                        "mysqld": { enabled: true, ensureRunning: true },
+                    },
+                },
+            },
+            "configure_mysql": {
+                commands: {
+                    "01_set_mysql_root_password": {
+                        command: `mysqladmin -u root password '${config.dbRootPassword}'`,
+                        test: `$(mysql ${config.dbName} -u root --password='${config.dbRootPassword}' >/dev/null 2>&1 </dev/null); (( $? != 0 ))`,
+                    },
+                    "02_create_database": {
+                        command: `mysql -u root --password='${config.dbRootPassword}' < /tmp/setup.mysql`,
+                        test: `$(mysql ${config.dbName} -u root --password='${config.dbRootPassword}' >/dev/null 2>&1 </dev/null); (( $? != 0 ))`,
+                    },
+                    "03_cleanup": {
+                        command: "rm /tmp/setup.mysql",
+                    },
+                },
+            },
+            "install_application": {
+                files: {
+                    "/tmp/database.yml": {
+                        content: renderConfigFile("./files/database.yml", config),
+                        mode: "000400",
+                        owner: "root",
+                        group: "root",
+                    },
+                    "/tmp/install_application": {
+                        content: renderConfigFile("./files/install_application", config),
+                        mode: "000500",
+                        owner: "root",
+                        group: "root",
+                    },
+                    "/home/ec2-user/start_application": {
+                        content: renderConfigFile("./files/start_application", config),
+                        mode: "000500",
+                        owner: "root",
+                        group: "root",
+                    },
+                },
+                commands: {
+                    "01_install_application": {
+                        command: "/tmp/install_application > /var/log/install_application.log",
+                    },
+                    "02_configure_reboot": {
+                        command: "echo /home/ec2-user/start_application >> /etc/rc.local",
+                    },
+                    "03_start_application": {
+                        command: "/home/ec2-user/start_application > /var/log/start_application.log",
+                    },
+                    "04_cleanup": {
+                        command: "rm /tmp/install_application",
+                    },
+                },
+            },
+        },
+    ),
 });
 
 // Export the VM IP in case we want to SSH.
-export const vmIP = webServer.publicIp;
+export let vmIP = webServer.publicIp;
+
+// Export the URL for our newly created Rails application.
+export let websiteURL = pulumi.interpolate`http://${webServer.publicDns}/notes`;
+
+runTests();
